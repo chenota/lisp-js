84b63b8201f1a235dbc1417ae45b954ec01a5709
chenota <achenot@mines.edu>
2024-04-12 13:11:29 -0600

    Refactored fn names in js-env. Added associated frame num w/ each frame

--- src/js-env.lisp
+++ src/js-env.lisp
@@ -2,11 +2,20 @@
 
 (in-package :lisp-js)
 
-(defparameter *stack* '(nil))
+(defparameter *stack* (list (cons nil 1)))
 
 ;; Push a new empty frame onto stack
-(defun push-frame nil 
-    (setq *stack* (cons nil *stack*)))
+(defun push-empty-frame (&key reset-frame-num) 
+    (setq 
+        *stack* 
+        (cons 
+            (cons 
+                nil 
+                ;; Reset frame num back to one if specified
+                (if reset-frame-num
+                    1
+                    (+ 1 (cdar *stack*))))
+            *stack*)))
 
 ;; Pop frame off of stack
 (defun pop-frame nil 
@@ -15,13 +24,13 @@
         (setq *stack* (cdr *stack*))))
 
 ;; Push variable onto current stack frame
-(defun push-current-frame (name value)
-    (setf (first *stack*)
+(defun push-to-current-frame (name value)
+    (setf (caar *stack*)
         (cons 
             ;; New conscell associating name to value
             (cons name value) 
             ;; Cons new key-value onto first stack frame
-            (first *stack*))))
+            (caar *stack*))))
 
 ;; Search a single stack frame
 (defun search-frame (frame name)
@@ -33,7 +42,7 @@
                     (equal (car new-var) name))
                 (cdr new-var)
                 acc))
-        frame 
+        (car frame) 
         :initial-value nil))
 
 (defun search-current-frame (name)
@@ -51,25 +60,39 @@
         *stack*
         :initial-value nil))
 
-(defun compress-stack nil 
-    (reverse
-        (caar
-            (reduce 
-                ;; New is a conscell (name, value), acc is a conscell of (frame, namelist)
-                (lambda (acc new)
-                    (if  
-                        ;; Check if variable has already been added to frame (already in namelist)
-                        (member (car new) (cdr acc))
-                        ;; If already found, ignore
-                        acc
-                        ;; If not found, add to compressed frame and namelist
-                        (cons (cons new (car acc)) (cons (car new) (cdr acc)))))
-                *stack* 
-                :initial-value (cons nil nil)))))
+;; Compress whole stack into one frame, remove duplicate variables
+(defun compress-stack (&key frame-num) 
+    (cons 
+        ;; Variable list
+        (first (reduce 
+            (lambda (compressed-stack new-frame)
+                (let 
+                    ((frame-vars 
+                        (reduce
+                            (lambda (compressed-frame new-var)
+                                (if  
+                                    ;; Check if variable already in stack
+                                    (member (car new-var) (cdr compressed-stack) :test 'string=)
+                                    ;; If so don't do anything
+                                    compressed-frame 
+                                    ;; If not, add to compressed frame
+                                    (cons new-var compressed-frame)))
+                            ;; Only care about var list, not frame no.
+                            (car new-frame)
+                            :initial-value nil)))
+                    ;; Update compressed stack w/ new variables
+                    (cons 
+                        ;; New variables on stack
+                        (append frame-vars (car compressed-stack)) 
+                        ;; Store new variable names so don't pull again
+                        (reduce (lambda (new acc) (cons (first new) acc)) frame-vars :initial-value (cdr compressed-stack)))))
+            *stack*
+            :initial-value (cons nil nil)))
+        ;; Frame number, return one if not specified
+        (if frame-num frame-num 1)))
 
-(defun push-new-frame (frame)
-    (setq *stack* 
-        (cons frame *stack*)))
+(defun push-frame (frame)
+    (setq *stack* (cons frame *stack*)))
 
 (defparameter *heap* nil)
 
